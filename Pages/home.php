b98a241e2f25c14adc0150a4aaa61847a8862fc9
Moad <moad.karame@gmail.com>
2024-04-27 20:24:29 +0100

    Home page structure.

--- Pages/home.php
+++ Pages/home.php
@@ -8,6 +8,26 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
-    
+    <div class=" grid grid-cols-5 h-screen max-h-screen">
+        <div class=" col-start-1 col-end-3 grid grid-rows-10">
+            <div class=" row-start-1 row-end-3 bg-slate-900">
+
+            </div>
+            <div class=" row-start-3 row-span-8 bg-slate-500">
+
+            </div>
+        </div>
+        <div class=" col-start-3 col-span-3 grid grid-rows-10">
+            <div class=" row-start-1 row-end-2 bg-slate-700">
+
+            </div>
+            <div class=" row-start-2 row-span-8 bg-slate-300">
+
+            </div>
+            <div class=" row-start-10 row-span-1 bg-slate-400">
+
+            </div>
+        </div>
+    </div>
 </body>
 </html>
